07c25d0c81a14b1812502102d84803506dedd2a0
Thomas Schnuerer <Thomas.Schnuerer@men.de>
2016-05-04 15:10:36 +0200

    R: emo1_*.h files for 10EM01-61 were missing

---
+++ em01_cfg.h
@@ -0,0 +1,261 @@
+/***********************  I n c l u d e  -  F i l e  ************************
+ *
+ *         Name: em01_cfg.h
+ *
+ *       Author: kp
+ *        $Date: 2011/05/23 15:31:26 $
+ *    $Revision: 3.16 $
+ *
+ *  Description: MEN EM01 Configuration and Memory Map
+ *
+ *     Switches: TGT_HAM to setup the HAMILTON MM FLASH mapping
+ *
+ *-------------------------------[ History ]---------------------------------
+ *
+ * $Log: em01_cfg.h,v $
+ * Revision 3.16  2011/05/23 15:31:26  ts
+ * R: new Variant EM01A09 ordered by customer with 64 MHz PCI CLK
+ * M: added defines to support 64 MHz PCI Clock in MPC
+ *
+ * Revision 3.15  2008/06/30 20:11:00  CSchuster
+ * R: from EM1N and EM1A R1 1.2V is connected to LM81 as well
+ * M: add LM81 fullScale value for 1.2V (EM01CFG_LM81_VCCP_2_FULLSCALE_MV)
+ *
+ * Revision 3.14  2008/06/27 15:00:27  ufranke
+ * R: cosmetics
+ * M: added _MV to EM01CFG_LM81_XXXX_FULLSCALE_MV to document measure is in [mV]
+ *
+ * Revision 3.13  2008/06/26 10:30:16  cs
+ * R: LM81 full scale values differed slightly from MenMon to BSPs
+ * M: define common LM81 full scale values here (EM01CFG_LM81_XXX)
+ *
+ * Revision 3.12  2008/02/04 14:42:54  ufranke
+ * added
+ *  + MM_PARAM_FPGA_LOC
+ *
+ * Revision 3.11  2007/10/19 13:30:40  ufranke
+ * added
+ *  + switch TGT_HAM to support the HAMILTON MM FLASH mapping
+ *    primary/secondary MENMON size 512 kB each
+ *    secondary MENMON start 0xfff80000
+ *
+ * Revision 3.10  2007/03/26 16:17:35  ufranke
+ * changed
+ *  - primary   MENMON size 256 kB
+ *  - secondary MENMON size 768 kB
+ *  - secondary MENMON start 0xfff40000
+ *
+ * Revision 3.9  2006/05/17 09:08:28  ufranke
+ * added
+ *  + define for 256MB SDRAM
+ *
+ * Revision 3.8  2006/05/09 14:42:47  UFRANKE
+ * moved/renamed following defines to COM/mgt5200.h
+ *  - EM01CFG_XLB_ARB_PRIO_EN  to MGT5200_XLB_ARB_PRIO_EN
+ *  - EM01CFG_XLB_ARB_PRIO_REG to MGT5200_XLB_ARB_PRIO_REG
+ *
+ * Revision 3.7  2006/04/28 15:28:12  ts
+ * + XLB Arbiter Registers to set priority scheme on XLB
+ *
+ * Revision 3.6  2006/02/22 18:18:20  UFRANKE
+ * added
+ *  + EM01CFG_FRAM_ADDR/SIZE
+ * changed
+ *  - FRAM will be mapped to f1000000
+ *    differs from SRAM address
+ *
+ * Revision 3.5  2005/09/09 20:17:49  CSchuster
+ * added defines for boot flash > 2MB
+ *
+ * Revision 3.4  2005/06/24 08:19:28  kp
+ * Copyright line changed (sbo)
+ *
+ * Revision 3.3  2005/04/12 16:56:07  kp
+ * + EM01CFG_MM_PARA_STRING_SIZE
+ *
+ * Revision 3.2  2005/03/16 16:51:00  kp
+ * completely reworked
+ *
+ * Revision 3.1  2005/03/04 10:36:32  kp
+ * Initial Revision
+ *
+ * cloned from km01_cfg.h Revision 3.1
+ *---------------------------------------------------------------------------
+ * (c) Copyright 2005..2005 by MEN Mikro Elektronik GmbH, Nuremberg, Germany
+ ****************************************************************************/
+
+#ifndef _EM01CFG_H
+#define _EM01CFG_H
+
+
+/*--------------------------------------+
+|   DEFINES                             |
++--------------------------------------*/
+
+/* clocks */
+#define EM01CFG_MAIN_CLK_32_0MHZ		32000000	/* 32MHz - good for CAN */
+
+#define EM01CFG_IPB_CLK_DIV			2			/* IPB = XLB/2 */
+#define EM01CFG_PCI_CLK_DIV			4           /* PCI = XLB/4 */
+#define EM01CFG_PCI_CLK_DIV_66MHZ	0           /* PCI = IPB   */
+
+#define EM01CFG_IIC_ONBOARD		0
+#define EM01CFG_IIC_IO			1
+
+/*
+ * CS Start/Size
+ */
+
+/* SDRAM */
+#define EM01CFG_DRAM_128MB			0x08000000
+#define EM01CFG_DRAM_256MB			0x10000000
+
+/* MGT5100 internal map */
+#define EM01CFG_MBAR_ADDR				0xf0000000 /* must match linker script */
+
+/* SRAM */
+#define EM01CFG_SRAM_ADDR				0xF2000000
+#define EM01CFG_SRAM_SIZE				0x00200000
+
+/* FRAM */
+#define EM01CFG_FRAM_ADDR				0xF1000000
+#define EM01CFG_FRAM_SIZE				0x00020000  /* 128 kB */
+
+/* CPLD */
+#define EM01CFG_CPLD_ADDR				0xF4000000
+#define EM01CFG_CPLD_SIZE				0x00010000
+
+#define EM01_CPLD_RCR	0x0000       /* reset control register address */
+#define EM01_CPLD_RCR_WD_TRIG	0x80 /* w: trigger bit */
+#define EM01_CPLD_RCR_LM81_INT	0x40 /* r: reset cause LM81 int */
+#define EM01_CPLD_RCR_SWRESET 	0x20 /* r: EM01_CPLD_MCR_DO_RESET executed */
+#define EM01_CPLD_RCR_POWER_ON 	0x10 /* r: reset cause power on */
+#define EM01_CPLD_RCR_TEMP_CRIT	0x08 /* r: reset cause LM81 temperature */
+#define EM01_CPLD_RCR_PB_RESET	0x04 /* r: reset cause PB RESET */
+#define EM01_CPLD_RCR_HRESET	0x02 /* r: reset cause HRESET */
+#define EM01_CPLD_RCR_WD		0x01 /* r: reset cause watchdog */
+
+/* CPLD MCR register defs */
+#define EM01_CPLD_MCR					0x1	/* control reg for FPGA */
+
+#define EM01_CPLD_MCR_FPGA_INIT_DONE		0x80 /* r */
+#define EM01_CPLD_MCR_FPGA_DATA				0x80 /* w */
+
+#define EM01_CPLD_MCR_FPGA_CLK				0x40 /* w */
+#define EM01_CPLD_MCR_FPGA_CONF_STATUS		0x40 /* r */
+
+#define EM01_CPLD_MCR_FPGA_CONFIG			0x20 /* w */
+
+#define EM01_CPLD_MCR_WD_TIME_MASK			0x0c /* r/w */
+#define EM01_CPLD_MCR_WD_120S				0x0
+#define EM01_CPLD_MCR_WD_50S				0x1
+#define EM01_CPLD_MCR_WD_20S				0x2
+#define EM01_CPLD_MCR_WD_1S8				0x3
+
+#define EM01_CPLD_MCR_WD_DIS				0x02 /* r/w */
+
+#define EM01_CPLD_MCR_NON_SAFETY_MODE		0x01 /* r */
+#define EM01_CPLD_MCR_DO_RESET				0x01 /* w */
+
+/* FPGA */
+#define EM01CFG_FPGA_PCI_DEV_NO		0x1D
+#define EM01CFG_FPGA_PCI_BUS		0
+#define EM01CFG_FPGA_PCI_FUNC		0
+
+/*
+ * FLASH
+ */
+/* boot flash */
+#define EM01CFG_FLASH_2MB					0xFFE00000
+#define EM01CFG_FLASH_SIZE_2MB				0x00200000
+#define EM01CFG_FLASH_4MB					0xFFC00000
+#define EM01CFG_FLASH_SIZE_4MB				0x00400000
+#define EM01CFG_FLASH_8MB					0xFF800000
+#define EM01CFG_FLASH_SIZE_8MB				0x00800000
+
+/* parameter string */
+#define EM01CFG_MM_PARA_FPGA_LOC_SAVE		0x40	/* information which FPGA was loaded will be stored here */
+#define EM01CFG_MM_PARA_FPGA_LOC_SAVE_SIZE	0x10    /* to hold this over reset */
+
+#define EM01CFG_MM_PARA_STRING_ADDR		0x3000
+#define EM01CFG_MM_PARA_STRING_SIZE		0x0400
+
+/* FLASH Mapping */
+#ifndef TGT_HAM
+	#define EM01CFG_PMM_BASE				0xFFF00000  /* must match linker script */
+	#define EM01CFG_SMM_BASE				0xFFF40000  /* must match linker script */
+	#define EM01CFG_PMM_SIZE				0x40000		/* 256 kB for PMM */
+	#define EM01CFG_SMM_SIZE				0xC0000		/* 768 kB for SMM */
+#else
+	#define EM01CFG_PMM_BASE				0xFFF00000  /* must match linker script */
+	#define EM01CFG_SMM_BASE				0xFFF80000  /* must match linker script */
+	#define EM01CFG_PMM_SIZE				0x80000		/* 512 kB for PMM */
+	#define EM01CFG_SMM_SIZE				0x80000		/* 512 kB for SMM */
+#endif /*TGT_HAM*/
+
+
+/*
+ * PCI - similar to MPC107 map B
+ */
+
+/* Entire PCI mem space */
+#define EM01CFG_PCI_MEM_START			0x80000000
+#define EM01CFG_PCI_MEM_SIZE			0x40000000
+
+/* PCI prefetchable mem space */
+#define EM01CFG_PCI_PF_MEM_START		EM01CFG_PCI_MEM_START
+#define EM01CFG_PCI_PF_MEM_SIZE			0x10000000
+
+/* PCI non prefetchable mem space */
+#define EM01CFG_PCI_NOPF_MEM_START		0x90000000
+#define EM01CFG_PCI_NOPF_MEM_SIZE		(EM01CFG_PCI_MEM_SIZE-\
+                                         EM01CFG_PCI_PF_MEM_SIZE)
+
+/* Onboard FPGA PCI mem space, prefetchable */
+#define EM01CFG_FPGA_PF_MEM_START		0x80000000
+#define EM01CFG_FPGA_PF_MEM_SIZE		0x02000000
+
+/* Onboard FPGA PCI mem space, non prefetchable */
+#define EM01CFG_FPGA_NOPF_MEM_START		EM01CFG_PCI_NOPF_MEM_START
+#define EM01CFG_FPGA_NOPF_MEM_SIZE		0x02000000
+
+
+
+/* PCI master window 1 */
+#define EM01CFG_PCI_ISA_MEM_START		0xFD000000
+#define EM01CFG_PCI_ISA_MEM_SIZE		0x01000000
+/* PCI master window 2 - shared between PCI IO and PCI CFG */
+#define EM01CFG_PCI_IO_CFG_START    	0xFE000000
+#define EM01CFG_PCI_IO_CFG_SIZE			0x00010000
+
+#define EM01CFG_PCI2MEM_1_START			0x00000000 /* PCI to SDRAM */
+#define EM01CFG_PCI2MEM_1_SIZE			0x40000000
+#define EM01CFG_PCI2MEM_0_SIZE			0x00040000
+#define EM01CFG_PCI2MEM_0_START			(EM01CFG_PCI2MEM_1_START \
+                                 - (EM01CFG_PCI2MEM_0_SIZE))/* PCI to MBAR */
+
+/* PCI IRQ mapping, FAKE NUMBERS are used */
+#define EM01CFG_PCI_IRQ_INTA	0xf0
+#define EM01CFG_PCI_IRQ_INTB	0xf1
+#define EM01CFG_PCI_IRQ_INTC	0xf2
+#define EM01CFG_PCI_IRQ_INTD	0xf3
+
+#define EM01CFG_PCI_DEVNO_MPC5200	0x0a	/* IDSEL0 = AD31 - MPC5200 special mapping */
+
+#define EM01CFG_LM81_2_5V_FULLSCALE_MV		(2500*255/192)	/* in mV */
+#define EM01CFG_LM81_VCCP_1_FULLSCALE_MV	(2700*255/192)	/* in mV */
+#define EM01CFG_LM81_3_3V_FULLSCALE_MV		(3300*255/192)	/* in mV */
+#define EM01CFG_LM81_5V_FULLSCALE_MV		(5000*255/192)	/* in mV */
+#define EM01CFG_LM81_VCCP_2_FULLSCALE_MV	(2700*255/192)	/* in mV */
+
+#endif /* _EM01CFG_H */
+
+
+
+
+
+
+
+
+
+
---
+++ em01_eeprom.h
@@ -0,0 +1,150 @@
+/***********************  I n c l u d e  -  F i l e  ************************
+ *
+ *         Name: em01_eeprom.h
+ *
+ *       Author: kp
+ *        $Date: 2008/06/30 20:10:58 $
+ *    $Revision: 3.6 $
+ *
+ *  Description: Layout of EM01 EEPROM
+ *
+ *     Switches: -
+ *
+ *-------------------------------[ History ]---------------------------------
+ *
+ * $Log: em01_eeprom.h,v $
+ * Revision 3.6  2008/06/30 20:10:58  CSchuster
+ * R: from EM1N and EM1A R1 1.2V is connected to LM81 as well
+ * M: add 1.2V calibration value to LM81_PARMS group
+ *
+ * Revision 3.5  2008/06/27 15:02:20  ufranke
+ * R: LM81 may be calibrated
+ * M: used 7 byte of reserved EEPROM space for calibration values
+ *
+ * Revision 3.4  2005/06/24 08:19:26  kp
+ * Copyright line changed (sbo)
+ *
+ * Revision 3.3  2005/04/12 16:56:05  kp
+ * removed unused defines, + EM01_SMB_SROM_VX
+ *
+ * Revision 3.2  2005/03/16 16:35:18  kp
+ * rewritten for 8kBit EEPROM
+ *
+ * Revision 3.1  2005/03/04 10:36:34  kp
+ * Initial Revision
+ *
+ *---------------------------------------------------------------------------
+ * (c) Copyright 2005..2005 by MEN Mikro Elektronik GmbH, Nuremberg, Germany
+ ****************************************************************************/
+#ifndef _EM01_EEPROM_H
+#define _EM01_EEPROM_H
+
+#ifdef __cplusplus
+	extern "C" {
+#endif
+
+/*--------------------------------------+
+|   DEFINES                             |
++--------------------------------------*/
+
+#define EM01_SMB_SROM_SIZE	(0x400)
+#define EM01_SMB_SROM_MM	0xa8
+#define EM01_SMB_SROM_VX	0xae /* at 0x300 in EEPROM (0x100 bytes) */
+
+/* get address of structure component */
+#ifndef offsetof
+	#define offsetof(type, part)	((int)((char*)&((type*)0)->part-(char*)0))
+#endif
+
+#define EM01_EEPROM_STARTUP_LEN		(0x180-0x70)
+#define EM01_KERPAR_LEN				0x180
+#define EM01_VXBLINE_LEN			0x100
+
+/* structure holding LM81 calibration parameters ( 7 byte ) */
+typedef	struct _LM81_PARMS
+{
+    u_int8      lm81_id;        	/* struct ID (0xA) and parity */
+    u_int8      lm81_2V5;
+    u_int8      lm81_3V3;
+    u_int8      lm81_5V0;
+    u_int8      lm81_vcore;
+    u_int8      lm81_1V2;
+    u_int8      lm81_res;			/* currently not used */
+} LM81_PARMS;
+
+
+/*
+ * EEPROM structure
+ * The entire structure covers 0x400 Byte (1KByte) (24C08)
+ */
+typedef struct {
+    /*--- offset 0x00: production data, hardware information (24 byte) ---*/
+	EEPROD2 pd;
+
+    /*--- offset 0x18: MENMON parameters (68 bytes) ---*/
+    struct menm_parms
+    {
+        u_int8      mm_id;        	/* struct ID (0x5) and parity */
+		u_int8      mm_misc;      	/* misc flags */
+		u_int8      mm_selftest;  	/* selftest options */
+		u_int8		mm_tto;			/* network timeout */
+
+        u_int32     mm_cbaud;     	/* console baudrate */
+        u_int32     mm_bsadr;     	/* address of bootstrapper */
+
+		u_int16		mm_vmode;		/* gfx video mode */
+		int16	   	mm_tdp;			/* telnet daemon port */
+		int16	   	mm_hdp;			/* HTTP daemon port */
+		int16		mm_wdt;			/* watchdog timer setting ???*/
+
+		u_int8		mm_stwait;		/* selftest wait time */
+		u_int8	   	mm_gcon;		/* gfx console select */
+		u_int8	   	mm_con[4];		/* console selectors  */
+		u_int8	   	mm_ecl;			/* ethernet I/F select */
+		int8		mm_tries; 		/* network retries
+									   -1 default = 20, 0 forever or 1..127 */
+		u_int8		mm_nspeed[3];	/* ethernet speed */
+
+		u_int16		mm_usr[16];		/* user defined */
+		u_int8		mm_powerReset; 	/* power supervision reset flags  */
+		u_int8		_resvd[3];		/* reserved */
+    } menm;
+
+	/*--- offset 0x5C: LM81 calibration parameters ( 7 byte ) ---*/
+	LM81_PARMS lm81;
+
+	/*--- offset 0x63: reserved ( 13 byte ) ---*/
+	char _rsvd[13];
+
+	/*--- offset 0x70: MENMON startup string (0x180-(pd+menm)) ---*/
+	char	startup[EM01_EEPROM_STARTUP_LEN];
+
+	/*--- offset 0x180: Linux kernel parameters (used by MENMON and Linux)*/
+	char	kerpar[EM01_KERPAR_LEN];
+
+	/*--- offset 0x300: vxWorks Bootline string (used by MENMON and VxWorks)*/
+	char	vxBline[EM01_VXBLINE_LEN];
+
+} EM01_EEPROM;
+
+/*
+ * Struct Ids
+ */
+#define EEID_PD      		0xd
+#define EM01_EEID_MENM    	0x5
+#define EM01_EEID_LM81    	0xA   /* LM81 calibration values */
+
+
+#ifdef __cplusplus
+	}
+#endif
+
+#endif /* _EM01_EEPROM_H */
+
+
+
+
+
+
+
+
