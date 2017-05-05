f80494c31b54a4e04b8a2b03b77c3a0aad6a1b47
thomas.schnuerer <thomas.schnuerer@men>
2017-05-05 14:40:28 +0200

    added GPL license notification under every MEN document header

--- chameleon.h
+++ chameleon.h
@@ -20,7 +20,21 @@
  * M: moved minRevision at end of struct, replaced reserved member with it
  *---------------------------------------------------------------------------
  * (c) Copyright 2015 by MEN Mikro Elektronik GmbH, Nuremberg, Germany
- ****************************************************************************/
+ ******************************************************************************/
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef _CHAMELEON_H
 #define _CHAMELEON_H
--- esmx_cb_eeprom.h
+++ esmx_cb_eeprom.h
@@ -20,7 +20,21 @@
  * cloned from esm_cb_eeprom.h (Revision 3.2  2007/07/11 14:37:54)
  *---------------------------------------------------------------------------
  * (c) Copyright 2008 by MEN Mikro Elektronik GmbH, Nuremberg, Germany
- ****************************************************************************/
+ ******************************************************************************/
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef _ESMX_CB_EEPROM_H
 #define _ESMX_CB_EEPROM_H
 
